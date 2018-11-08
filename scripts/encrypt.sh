651a3996009689981dc6bf223b5dcb387414696f
fenstamaker <gary.fenstamaker@washpost.com>
2018-11-08 15:57:31 -0500

    Add support for aliases in KMS

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+usage="$(basename "$0") [-r] [-k n] -- program to calculate the answer to life, the universe and everything
+
+where:
+    -h  show this help text
+    -s  set the seed value (default: 42)"
+
+
 if [ -z $AWS_REGION ]; then
     AWS_REGION="us-east-1"
 fi
@@ -11,19 +18,22 @@ while getopts ":r:k:" o; do
             ;;
         k)
             shift
-            key=${OPTARG}
+            k=${OPTARG}
             ;;
     esac
     shift
 done
 
 key_region=$(echo $AWS_REGION | tr '[:lower:]' '[:upper:]' | tr "-" "_")
-key_name=$(echo $key | tr '[:lower:]' '[:upper:]' | tr "-" "_")
+key_name=$(echo $k | tr '[:lower:]' '[:upper:]' | tr "-" "_")
 key_path="${key_region}_${key_name}"
 
 source ~/.envs/.kms || exit 1
 
 key=${!key_path}
+if [ -z key ]; then
+    key="alias/${k}"
+fi
 
 if [ -z "$1" ]; then
     if [ -t 0 ]; then
