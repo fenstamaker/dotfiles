735da5dee5c9b6b86b3e7e3896b886808dfdba50
Gary Fenstamaker <gary.fenstamaker@washpost.com>
2019-06-27 16:14:34 -0400

    Remove emacs directory

--- emacs/emacs.d/emacs.d.bak/lisp/_init-template.el
+++
@@ -1,7 +0,0 @@
-;;; init-theme.el --- Load theme configuration -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-
-(provide 'init-theme)
-;;; init-theme.el ends here
--- emacs/emacs.d/emacs.d.bak/lisp/init-groovy.el
+++
@@ -1,9 +0,0 @@
-;;; init-groovy.el --- Load groovy configuration -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(use-package groovy-mode
-  :ensure t)
-
-(provide 'init-groovy)
-;;; init-groovy.el ends here
